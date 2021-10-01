51789fc5d3ada1c088d06dfdb6e6a4ab717174e6
Matheus <79723385+pitouemat@users.noreply.github.com>
2021-10-01 20:08:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client, Intents, MessageAttachment, Message } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const token = "ODc3MzE5MjMyNDI4MzE4Nzgx.YRw5NA.9683Ai2F_pxcRzNwuc3CTr1qITM";
+const fetch = require('node-fetch');
+
+client.on("message", (msg) => {
+    if (msg.author.bot === true) return
+    if (msg.content.startsWith("H!")) {
+
+        let query = msg.content.substring(2).replace(' ', '');
+        const api = `https://api.jikan.moe/v3/search/anime?q=${query}`
+
+        fetch(api)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(dados) {
+                const data = dados.results[0];
+
+                const title = data.title;
+                const img = data.image_url;
+                const score = data.score;
+                const episodes = data.episodes;
+                const members = data.members;
+                const id = data.mal_id;
+
+                const url_id = `https://api.jikan.moe/v3/anime/${id}`;
+
+                fetch(url_id)
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(dados) {
+                        const sinopse = dados.synopsis
+                        const pop = dados.popularity
+                        const rank = dados.rank;
+                        const favorites = dados.favorites;
+
+                        msg.reply(
+                            `**${title}** \n${sinopse} \n \nScore ⭐: ${score}            Episódios 📺: ${episodes}            Membros🧍: ${members}     \nFavoritos 💓: ${favorites}       Rank 🏆: ${rank}              Popularity  📣: ${pop}`)
+                    })
+            });
+    }
+})
+
+client.login(token);
+console.log("Estou Pronta!");
